d6f71512d0bef474d591e497cc2bec66594c0148
Babak Fard <babak.jfard@gmail.com>
2016-06-07 11:15:13 -0400

    created file reduceAbundances.R

--- reduceAbundances.R
+++ reduceAbundances.R
@@ -1,3 +1,17 @@
-# This file averages the abundances of different variations of the same scenario in 1 file
+#================== 6/7/2016
+# to average different variations of one condition into one file
+theFiles <- list.files(path = "./Output")
+
+# put them into collections, so must end up having 5x5x4 = 100 different cases
+# 5 no.species <- {5, 10, 20, 50, 100}, 5 connectances <-{0.05, 0.1, 0.18, 0.25, 0.3}, 4 patch.numbers <- {2,  5, 15, 50}
+S <-  as.character(c(5, 10, 20, 50, 100))  # No. of species
+C <- c("05", "1", "18", "25", "3")   # connectance values
+P <- as.character(c(2, 5, 15, 50))   # number of patches
+combinations <- expand.grid(S, C, P)
+
+grep(paste0("S",S,"C",C,"n_V[[:digit:]]+_P",P,".rda"), theFiles, value = TRUE)
+
+# each collections contains arrays with equal sizes (not more than 30 arrays in each collection)
+# the array of mean values for each collection is calculated!!# This file averages the abundances of different variations of the same scenario in 1 file
 # so there will be (5species x 5 connectance x 4 patches)= 100 files
 
