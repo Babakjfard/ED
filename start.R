f6f8d728fd825de24baa9984ad71ea2456f84f21
Babak Fard <babak.jfard@gmail.com>
2019-07-05 15:31:00 -0400

    Update start.R

--- start.R
+++ start.R
@@ -1,3 +1,5 @@
+# The file to run on my local computer.
+# Goes throught the different created food webs in \Input and runs to create stability vs. synchrony calculations
 files <- list.files("/Volumes/Macintosh\ HD\ 2/Projects/ecological/ED/Input")
 #files <- c("S5C05n_V1_P2.rda", "S5C05n_V1_P5.rda", "S5C05n_V11_P2.rda","S5C05n_V11_P5.rda")
 for (the.names in files){
